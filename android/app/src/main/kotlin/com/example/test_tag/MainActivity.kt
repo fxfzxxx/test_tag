4a080f0802925659ef6dd26e38f4cf77c564036d
Aaron Fan <aaron1995228@gmail.com>
2023-09-19 17:12:43 +1200

    Initialized the project with some skeleton.

---
+++ android/app/src/main/kotlin/com/example/test_tag/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.test_tag
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/utils/app_layout.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class AppLayout{
+  static getSize(BuildContext context){
+    return MediaQuery.of(context).size;
+  }
+  static const int designWidth = 393;
+  static const int designHeight = 852;
+  static getScreenHeight(){
+    return Get.height;
+  }
+  static getScreenWidth(){
+    return Get.width;
+  }
+  static getHeight(double height){
+    return (height/designHeight)*getScreenHeight();
+  }
+  static getWidth(double width){
+    return (width/designWidth)*getScreenWidth();
+  }
+}
