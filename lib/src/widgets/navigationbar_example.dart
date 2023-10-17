e6abc1d8f59df7e9635b4973037f9481f96e1be6
Aaron Fan <aaron1995228@gmail.com>
2023-10-17 16:57:26 +1300

    Set up the Appbar and BottomNavigationbar.

---
+++ lib/src/widgets/navigationbar_example.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class NavigationBarExample extends StatefulWidget {
+  const NavigationBarExample({super.key});
+
+  @override
+  State<NavigationBarExample> createState() => _NavigationBarExampleState();
+}
+
+class _NavigationBarExampleState extends State<NavigationBarExample> {
+  int currentPageIndex = 0;
+  final destinationArray = [
+    const NavigationDestination(
+      icon: Icon(Icons.home),
+      selectedIcon: Icon(Icons.home),
+      label: 'Home',
+    ),
+    const NavigationDestination(
+      icon: Icon(Icons.search),
+      selectedIcon: Icon(Icons.search),
+      label: 'Search',
+    ),
+    const NavigationDestination(
+      icon: Icon(Icons.favorite_border),
+      selectedIcon: Icon(Icons.favorite),
+      label: 'Favorites',
+    ),
+    const NavigationDestination(
+      icon: Icon(Icons.person_outline),
+      selectedIcon: Icon(Icons.person),
+      label: 'Profile',
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(destinationArray[currentPageIndex].label),
+      ),
+      bottomNavigationBar: NavigationBar(
+          onDestinationSelected: (int index) {
+            setState(() {
+              currentPageIndex = index;
+            });
+          },
+          indicatorColor: Colors.blue,
+          selectedIndex: currentPageIndex,
+          destinations: destinationArray),
+      body: <Widget>[
+        Container(
+          color: Colors.red,
+          child: const Center(
+            child: Text('Home'),
+          ),
+        ),
+        Container(
+          color: Colors.green,
+          child: const Center(
+            child: Text('Search'),
+          ),
+        ),
+        Container(
+          color: Colors.blue,
+          child: const Center(
+            child: Text('Favorites'),
+          ),
+        ),
+        Container(
+            color: Colors.yellow,
+            child: const Center(
+              child: Text('Profile'),
+            ))
+      ][currentPageIndex],
+    );
+  }
+}
