ef645837b4fd0a32511066d5f887b6cd5f7af600
Aaron Fan <aaron1995228@gmail.com>
2023-11-08 17:25:14 +1300

    Make the tagList model to display single tag instead of the tag object with range ID.
    Created the table view in the report screen.

--- lib/src/model/tag_list.dart
+++ lib/src/model/tag_list.dart
@@ -5,12 +5,12 @@ import 'package:test_tag/src/model/tag.dart';
 
 class TagListModel extends ChangeNotifier{
   
-  List<TagModel> tags;
+  List<SingleTag> tags;
 
   TagListModel(this.tags);
 
   void addTag(TagModel tag) {
-    tags.add(tag);
+    addTagsFromRange(tag);
         notifyListeners();
   }
 
@@ -19,7 +19,7 @@ class TagListModel extends ChangeNotifier{
     notifyListeners();
   }
 
-  void updateTag(TagModel tag) {
+  void updateTag(SingleTag tag) {
     tags[tags.indexWhere((element) => element == tag)] = tag;
     notifyListeners();
   }
@@ -27,6 +27,44 @@ class TagListModel extends ChangeNotifier{
   void clearTags() {
     tags.clear();
   }
+
+  void addTagsFromRange(TagModel tag) {
+  // Separate all alphabetic and numeric parts
+  var matchesStart = RegExp(r'(\D+|\d+)').allMatches(tag.tagStartId).map((match) => match.group(0)).toList();
+  var matchesEnd = RegExp(r'(\D+|\d+)').allMatches(tag.tagEndId).map((match) => match.group(0)).toList();
+
+  // Check if the non-numeric parts are the same
+  if (matchesStart.where((s) => !RegExp(r'\d+').hasMatch(s!)).join() == matchesEnd.where((s) => !RegExp(r'\d+').hasMatch(s!)).join()) {
+    // Find the last numeric part in startID and the corresponding part in endID
+    for (var i = matchesStart.length - 1; i >= 0; i--) {
+      if (RegExp(r'\d+').hasMatch(matchesStart[i]!)) {
+        var numberStart = int.parse(matchesStart[i]!);
+        var numberEnd = int.parse(matchesEnd[i]!);
+
+        // Create SingleTag instances for each ID in the range
+        for (var j = numberStart; j <= numberEnd; j++) {
+          matchesStart[i] = j.toString();
+          var newTag = SingleTag(
+            id: matchesStart.join(''),
+            description: '',
+            tagOwner: tag.tagOwner,
+            tagName: tag.tagName,
+            tagLocation: tag.tagLocation,
+            tagStatus: tag.tagStatus,
+            startTime: tag.startTime,
+            endTime: tag.endTime,
+            // Copy other properties from the original tag
+          );
+          tags.add(newTag);
+        }
+
+        break;
+      }
+    }
+  }
+
+  notifyListeners();
+}
 
   int get length => tags.length;
 
@@ -45,3 +83,23 @@ class TagListModel extends ChangeNotifier{
     return compute(parseTags, response.body);
   }
 }
+class SingleTag {
+  String id;
+  String description;
+  String tagOwner;
+  String tagName;
+  String tagLocation;
+  bool tagStatus;
+  DateTime startTime;
+  DateTime endTime;
+  SingleTag(
+      {required this.id,
+      required this.description,
+      required this.tagOwner,
+      required this.tagName,
+      required this.tagLocation,
+      required this.tagStatus,
+      required this.startTime,
+      required this.endTime});
+}
+
--- lib/src/screens/report_screen.dart
+++ lib/src/screens/report_screen.dart
@@ -1,53 +1,78 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:test_tag/src/model/tag.dart';
 import 'package:test_tag/src/model/tag_list.dart';
 
 class ReportScreen extends StatelessWidget {
-  const ReportScreen({super.key});
+  const ReportScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     var tagList = context.watch<TagListModel>();
-    return ListView.builder(
-      itemCount: tagList.tags.length,
-      itemBuilder: (context, index) {
-        return ListTile(
-          title: Text(tagList.tags[index].tagName),
-          subtitle: Text(tagList.tags[index].tagLocation),
-          // onTap: () {
-          //   Navigator.pushNamed(context, '/report1');
-          // },
-        );
-      },
-      // .reports.map((report) {
-      //   return ListTile(
-      //     title: Text(report.title),
-      //     onTap: () {
-      //       Navigator.pushNamed(context, report.route);
-      //     },
-      //   );
-      // }).toList(),
-      // children: <Widget>[
-      //   ListTile(
-      //     title: const Text('Report 1'),
-      //     onTap: () {
-      //       Navigator.pushNamed(context, '/report1');
-      //     },
-      //   ),
-      //   ListTile(
-      //     title: const Text('Report 2'),
-      //     onTap: () {
-      //       Navigator.pushNamed(context, '/report2');
-      //     },
-      //   ),
-      //   ListTile(
-      //     title: const Text('Report 3'),
-      //     onTap: () {
-      //       Navigator.pushNamed(context, '/report3');
-      //     },
-      //   ),
-      // ],
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 8, top: 8.0),
+          child: Table(
+            columnWidths: const {
+              0: FlexColumnWidth(1.0), // Flex factor for 'ID' column
+              1: FlexColumnWidth(0.8), // Flex factor for 'Name' column
+              2: FlexColumnWidth(0.8), // Flex factor for 'Location' column
+              3: FlexColumnWidth(0.5), // Flex factor for 'Status' column
+            },
+            children: const [
+              TableRow(
+                children: [
+                  Text('ID'),
+                  Text('Name'),
+                  Text('Location'),
+                  // Text('Description'),
+                  Text('Status'),
+                ],
+              ),
+            ],
+          ),
+        ),
+        const Divider(),
+        Expanded(
+          child: ListView.separated(
+            itemCount: tagList.tags.length,
+            itemBuilder: (context, index) {
+              var tag = tagList.tags[index];
+              return Container(
+                color: tag.tagStatus
+                    ? Colors.transparent
+                    : Colors.red[100], // Highlight if status is false
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 8),
+                  child: Table(
+                    columnWidths: const {
+                      0: FlexColumnWidth(1.0), // Flex factor for 'ID' column
+                      1: FlexColumnWidth(0.8), // Flex factor for 'Name' column
+                      2: FlexColumnWidth(
+                          0.8), // Flex factor for 'Location' column
+                      3: FlexColumnWidth(
+                          0.5), // Flex factor for 'Status' column
+                    },
+                    children: [
+                      TableRow(
+                        children: [
+                          Text(tag.id),
+                          Text(tag.tagName),
+                          Text(tag.tagLocation),
+                          // Text(tag.tagDescription),
+                          Text(tag.tagStatus ? 'True' : 'False'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+            separatorBuilder: (context, index) =>
+                const Divider(), // Add a divider between each item
+          ),
+        ),
+      ],
     );
   }
 }
